bd03cdfeb735a082c34226d04caf4dca3b13ec8d
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-23 09:01:26 +0200

    add documentation and default disk IDs

--- libsmart.py
+++ libsmart.py
@@ -4,8 +4,21 @@
 #
 
 class SmartDisk():
-  
+  """
+  A class to access information on S.M.A.R.T. disks.
+
+  Usage: (under construction)
+  """
 
 
 if __name__ == '__main__'
   # do something
+  sda=wwn-0x50026b723c0d6dd5
+  sdb=wwn-0x5000c50050a30da3
+  sdc=wwn-0x5000c50050a32d4f
+  #sdd=wwn-0x5000c50052c28f7d
+  sdd=wwn-0x50014ee6055a237b
+  #sde=wwn-0x5000c50050f3630b
+  sde=wwn-0x50014ee60507b79c
+
+  DEBUG = True
