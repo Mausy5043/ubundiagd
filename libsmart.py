3209c8d9ca00f0081031c1877b02cfd6c0dd632a
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-23 12:52:20 +0200

    more info from smart

--- libsmart.py
+++ libsmart.py
@@ -12,7 +12,11 @@ class SmartDisk():
   """
   def __init__(self,diskid):
     self.wwn = diskid
-    self.smartinfo = ""
+    self.vars     = "-"
+    self.health   = "-"
+    self.selftest = "-"
+    self.info     = "-"
+    self.identity = commands.getoutput("sudo smartctl -i /dev/disk/by-id/" + self.wwn).splitlines()
     self.lasttime = -1
 
   def smart(self):
@@ -20,15 +24,18 @@ class SmartDisk():
     # only read the S.M.A.R.T. data if current data is stale
     # data is considered stale if it is older than 4 minutes
     if ((t1 - self.lasttime) > (4*60)):
-      self.smartinfo = commands.getoutput("sudo smartctl -A /dev/disk/by-id/" + self.wwn).splitlines()
+      self.vars     = commands.getoutput("sudo smartctl -A /dev/disk/by-id/" + self.wwn).splitlines()
+      self.info     = commands.getoutput("sudo smartctl -i /dev/disk/by-id/" + self.wwn).splitlines()
+      self.health   = commands.getoutput("sudo smartctl -H /dev/disk/by-id/" + self.wwn).splitlines()
+      self.selftest = commands.getoutput("sudo smartctl -l selftest /dev/disk/by-id/" + self.wwn).splitlines()
       self.lasttime = t1
     else:
       if DEBUG:print "Using old data: "
-    if DEBUG:print self.smartinfo
+    if DEBUG:print self.vars
     return 0
 
-  def getinfo(self,id):
-    for line in self.smartinfo:
+  def getdata(self,id):
+    for line in self.vars:
       if (line != ''):
         ls=line.split()
         if (ls[0] == id):
@@ -36,6 +43,15 @@ class SmartDisk():
           ret=ls[9]
     return ret
 
+  def gethealth(self):
+    return "Disk health not available."
+
+  def getlasttest(self):
+    return "Most recent selftest info not available"
+
+  def getinfo(self):
+    ret = self.wwn
+    return ret
 
 
 if __name__ == '__main__':
@@ -44,16 +60,16 @@ if __name__ == '__main__':
 
   sda = SmartDisk("wwn-0x50026b723c0d6dd5")
   sda.smart()
-  print sda.getinfo('194')
+  print sda.getdata('194')
   sdb = SmartDisk("wwn-0x5000c50050a30da3")
   sdb.smart()
-  print sdb.getinfo('194')
+  print sdb.getdata('194')
   sdc = SmartDisk("wwn-0x5000c50050a32d4f")
   sdc.smart()
-  print sdc.getinfo('194')
+  print sdc.getdata('194')
   sdd = SmartDisk("wwn-0x50014ee6055a237b")
   sdd.smart()
-  print sdd.getinfo('194')
+  print sdd.getdata('194')
   sde = SmartDisk("wwn-0x50014ee60507b79c")
   sde.smart()
-  print sde.getinfo('194')
+  print sde.getdata('194')
