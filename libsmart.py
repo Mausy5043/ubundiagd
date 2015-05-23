5a8728db454872c78881440553984036638732ca
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-23 10:11:38 +0200

    20150523.1011

--- libsmart.py
+++ libsmart.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import commands
+
 DEBUG = False
 
 class SmartDisk():
@@ -12,7 +14,7 @@ class SmartDisk():
     self.wwn = diskid
 
   def smart(self):
-    self.smartcmd = "sudo smartctl -A /dev/disk/by-id/" + self.wwn
+    self.smartcmd = commands.getoutput("sudo smartctl -A /dev/disk/by-id/" + self.wwn)
     if DEBUG:print self.smartcmd
     return "OK"
 
@@ -24,9 +26,9 @@ if __name__ == '__main__':
   sda="wwn-0x50026b723c0d6dd5"
   sdb="wwn-0x5000c50050a30da3"
   sdc="wwn-0x5000c50050a32d4f"
-  #sdd="wwn-0x5000c50052c28f7d
+  #sdd="wwn-0x5000c50052c28f7d"
   sdd="wwn-0x50014ee6055a237b"
-  #sde=wwn-0x5000c50050f3630b
+  #sde="wwn-0x5000c50050f3630b"
   sde="wwn-0x50014ee60507b79c"
 
   DEBUG = True
