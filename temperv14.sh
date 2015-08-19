f36928fa788d290a23539e2abb7dc329119ea2a1
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-08-19 16:32:35 +0200

    only one datapoint in the file

--- temperv14.sh
+++ temperv14.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-of=/tmp/temperv14.dat
-lf=/tmp/temperv14.lock
+of="/tmp/temperv14.dat"
+lf="/tmp/temperv14.lock"
 
 touch $lf
-/srv/array1/rbin/boson/temperv14 -c >>$of
+/srv/array1/rbin/boson/temperv14 -c >$of
 chmod 777 $of
 rm $lf
