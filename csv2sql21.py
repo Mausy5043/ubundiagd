9128229fdd9013df0b013614e2e15684acd05b11
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-01-02 11:50:32 +0100

    20160102T1150

--- csv2sql21.py
+++ csv2sql21.py
@@ -17,24 +17,7 @@ def cat(filename):
     f.close()
   return ret
 
-def do_createtable(cnsql)
-  try:
-    cursql = cnsql.cursor()
-    cmd = ('SOURCE createtable21.sql')
-    cursql.execute(cmd)
-    cnsql.commit()
-    cursql.close()
-  except mdb.Error, e:
-    print("MySQL error")
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    if consql:    # attempt to close connection to MySQLdb
-      print("Closing MySQL connection")
-      consql.close()
-      print("Closed MySQL connection")
-    print(e.__doc__)
-    raise
-
-def do_writesample(cnsql, sample)
+def do_writesample(cnsql, sample):
   sample = sample.split(', ')
   sample_time = sample[0]
   sample_epoch = int(sample[1])
@@ -44,8 +27,8 @@ def do_writesample(cnsql, sample)
   cmd = ('INSERT INTO temper '
                     '(sample_time, sample_epoch, temperature) '
                     'VALUES (%s, %s, %s)')
-  if DEBUG: print cmd, "// result = ",result
-  dat = (t_sample[0], int(t_sample[1]), result )
+  dat = (sample_time, sample_epoch, temperature)
+  print cmd,dat
   cursql.execute(cmd, dat)
   cnsql.commit()
   cursql.close()
@@ -71,13 +54,8 @@ if __name__ == "__main__":
     print(e.__doc__)
     raise
 
-  do_createtable(consql)
-
   data = cat(infile).splitlines()
-  print data[-1].split(', ')
   for entry in range(0, len(data)):
-    print data[entry]
-    #do_writesample(consql,data[entry])
-  print data[0].split(', ')
-  print data[1].split(', ')
+    #print data[entry]
+    do_writesample(consql,data[entry])
   print data[-1].split(', ')
