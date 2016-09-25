a68a0d6a81aa5729af018b8fcea64b1c2d60f08d
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-09-25 11:25:00 +0200

    deprecating wind graph

--- testgraph.sh
+++ testgraph.sh
@@ -13,7 +13,7 @@ pushd $HOME/ubundiagd
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM temper where (sample_time) >=NOW() - INTERVAL 8 DAY;"   | sed 's/\t/;/g;s/\n//g' > /tmp/sql21c.csv
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM temper where (sample_time) >=NOW() - INTERVAL 32 DAY;"  | sed 's/\t/;/g;s/\n//g' > /tmp/sql21d.csv
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM temper where (sample_time) >=NOW() - INTERVAL 370 DAY;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql21e.csv
-  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM wind where (sample_time) >=NOW() - INTERVAL 50 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql29.csv
+ # mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM wind where (sample_time) >=NOW() - INTERVAL 50 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql29.csv
 
   touch /tmp/ubundiagd/graph.lock
 
@@ -21,8 +21,8 @@ pushd $HOME/ubundiagd
   gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21c.gp
   gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21d.gp
   gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21e.gp
-  gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph29.gp
-  ./graph29.py
+  #gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph29.gp
+  #./graph29.py
   chown beheer:users /tmp/*.png
   mv /tmp/plot*.png  /var/www/status/
   mv /tmp/again*.png /var/www/weer/
